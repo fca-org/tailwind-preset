710f312b7aee88f21d6e1faacb09e9080b56849d
Luke McDonald <lukemcdonald@fca.org>
2023-03-30 16:15:49 -0500

    fix: update colors

--- config/colors.js
+++ config/colors.js
@@ -3,19 +3,19 @@ const twColors = require('tailwindcss/colors');
 const palettes = {
   gray: [
     '#f8fafc', '#f1f5f9', '#e2e8f0', '#cbd5e1', '#94a3b8',
-    '#64748b', '#475569', '#334155', '#1e293b', '#0f172a',
+    '#64748b', '#475569', '#334155', '#1e293b', '#0f172a', '#020617'
   ],
   dignity: [
     '#e7f9ff', '#d3f4ff', '#afe8ff', '#7fd5ff', '#4db3ff',
-    '#258eff', '#0164ff', '#0056d6', '#003d99', '#041e42',
+    '#258eff', '#0164ff', '#0056d6', '#003d99', '#041e42', '#020d1d'
   ],
   integrity: [
     '#fdf3f3', '#fbeae8', '#f7d4d4', '#f0b2b1', '#e68686',
-    '#d95a5e', '#c43a45', '#a42c38', '#862633', '#6d222f',
+    '#d95a5e', '#c43a45', '#a42c38', '#862633', '#6d222f', '#46161e'
   ],
   grace: [
     '#fefce8', '#fef9c3', '#fef08a', '#fde047', '#facc15',
-    '#eaaa00', '#ca8a04', '#a16207', '#854d0e', '#713f12',
+    '#eaaa00', '#ca8a04', '#a16207', '#854d0e', '#713f12', '#422006'
   ]
 }
 
@@ -31,6 +31,7 @@ function generateColorPalette( name = '', colors = [], defaultColor = '' ) {
     700: `var(--color-${ name }-700, ${colors[7]})`,
     800: `var(--color-${ name }-800, ${colors[8]})`,
     900: `var(--color-${ name }-900, ${colors[9]})`,
+    950: `var(--color-${ name }-950, ${colors[10]})`,
     DEFAULT: `var(--color-${ name }, ${defaultColor})`,
   };
 }
@@ -38,8 +39,9 @@ function generateColorPalette( name = '', colors = [], defaultColor = '' ) {
 const colors = {
   white: 'var(--color-white, #ffffff)',
   black: 'var(--color-black, #000000)',
-  transparent: 'transparent',
+  inherit: 'inherit',
   current: 'currentColor',
+  transparent: 'transparent',
 
   gray: generateColorPalette( 'gray', palettes.gray, palettes.gray[7] ),
   dignity: generateColorPalette( 'dignity', palettes.dignity, palettes.dignity[9] ),
@@ -53,7 +55,7 @@ const colors = {
   },
   info: {
     DEFAULT: `var(--color-info, ${palettes.dignity[5]})`,
-    dark: `var(--color-info-700, ${palettes.dignity[7]})`,
+    dark: `var(--color-info-800, ${palettes.dignity[8]})`,
     light: `var(--color-info-100, ${palettes.dignity[1]})`
   },
   success: {
@@ -63,7 +65,7 @@ const colors = {
   },
   warning: {
     DEFAULT: `var(--color-warning, ${palettes.grace[5]})`,
-    dark: `var(--color-warning-800, ${palettes.grace[7]})`,
+    dark: `var(--color-warning-800, ${palettes.grace[8]})`,
     light: `var(--color-warning-100, ${palettes.grace[1]})`
   },
 }
